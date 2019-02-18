c42b683f22643928490de77f2982c9731c48c601
Nathaniel Riehl <nate@nateriehl.com>
2019-02-18 16:29:20 -0600

    Update Example files to use a mock api

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
+import {getUsers} from "./api/exampleApi";
 import "./scss/index.scss";
-// tslint:disable:no-console
 
-console.log("webpack starterkit");
+// Populate table of users via API call.
+getUsers().then((result) => {
+  let usersBody = "";
+
+  result.forEach((user) => {
+    // tslint:disable:semicolon
+    usersBody += `<tr>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+    // tslint:enable:semicolon
+  });
+
+  // @ts-ignore
+  global.document.getElementById("users").innerHTML = usersBody;
+});
